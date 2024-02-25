4992c4db472fbc103ade5c9247f4695830846a7a
jmxor <jamesmrippeth@gmail.com>
2024-02-25 10:43:27 +0000

    Add basic padding and step display

--- src/app/symmetric-cryptography/block-ciphers/des/structure/page.tsx
+++ src/app/symmetric-cryptography/block-ciphers/des/structure/page.tsx
@@ -6,16 +6,35 @@ import {motion, useCycle} from "framer-motion";
 
 export default function DESCipherPage() {
   const [animationStep, setAnimationStep] = useState(0);
-  const [block, setBlock] = useState('1001101011111011110100100110100011001010100101001000101100111101');
+  const [input, setInput] = useState('1001101011111011110100100110100011001010100101001000101100111101');
+  const [animationInput, setAnimationInput] = useState('')
+  const [block, setBlock] = useState('');
   const [isAnimated, cycleAnimated] = useCycle(false, true)
+
+  const startAnimation = () => {
+    setAnimationStep(0);
+    setAnimationInput(input);
+    setBlock(input);
+  }
 
   const stepForward = () => {
     setAnimationStep(animationStep + 1)
+
+    switch (animationStep) {
+      case 1:
+        // TODO: Change padding to actual padding algorithm
+        setBlock(animationInput.padEnd(64, '0'))
+        break;
+    }
   }
 
   const stepBackward = () => {
-    if (animationStep > 0) {
-      setAnimationStep(animationStep - 1)
+    if (animationStep == 0) return;
+
+    setAnimationStep(animationStep - 1)
+
+    switch (animationStep) {
+
     }
   }
 
@@ -41,44 +60,82 @@ export default function DESCipherPage() {
     62, 54, 46, 38, 30, 22, 14, 6,
   ];
 
-  // TODO: Add padding of input block
+  // TODO: fix spacing between bits and step animations
 
   return (
     <>
-      <section className="w-full flex flex-col items-center text-center sm:w-1/2">
-        <h1>Block</h1>
-        <div className="relative">
-          {/* Grid */}
-          <div className="grid grid-cols-8 border border-black">
-            {block.split('').map((b, k) => (
-              <div key={k} className="w-6 h-6 border border-black"/>
-            ))}
+      <section className="w-full flex flex-col sm:w-1/3">
+        <textarea
+          className="border border-black resize-none"
+          value={input}
+          onChange={e => setInput(e.target.value)}
+          maxLength={64}
+        />
+
+        <button
+          className="border my-2 border-black"
+          onClick={startAnimation}
+        >
+          Start Encryption
+        </button>
+
+        <div className="flex justify-between">
+          <div className="flex flex-col items-center">
+            <h1>Block</h1>
+            <div className="relative text-center">
+              {/* Grid */}
+              <div className="grid grid-cols-8 border border-black">
+                {Array(64).fill(null).map((_, k) => (
+                  <div key={k} className="w-6 h-6 border border-black"/>
+                ))}
+              </div>
+
+              {/* Bits */}
+              <div className="absolute top-0 left-0 grid grid-cols-8 border border-transparent font-mono">
+                {block.split('').map((b, k) => (
+                  <motion.div
+                    key={k}
+                    className="absolute w-6 h-6"
+                    animate={{
+                      x: ((animationStep > 3 ? IPTransform[k] : k) % 8) * 24,
+                      y: Math.floor((animationStep > 3 ? IPTransform[k] : k) / 8) * 24,
+                    }}
+                    transition={{ease: "easeInOut"}}
+                    initial={false}
+                  >
+                    {b}
+                  </motion.div>
+                ))}
+              </div>
+            </div>
           </div>
 
-          {/* Bits */}
-          <div className="absolute top-0 left-0 grid grid-cols-8 border border-transparent font-mono">
-            {block.split('').map((b, k) => (
-              <motion.div
-                key={k}
-                className="absolute w-6 h-6"
-                animate={{
-                  x: ((isAnimated ? IPTransform[k] : k) % 8) * 24,
-                  y: Math.floor((isAnimated ? IPTransform[k] : k) / 8) * 24,
-                }}
-                transition={{ease: "easeInOut"}}
-                initial={false}
-              >
-                {k}
-              </motion.div>
-            ))}
-          </div>
+          <ol className="flex flex-col mt-6 border border-black text-center">
+            <motion.li
+              className="h-6 border border-black"
+              animate={{backgroundColor: (animationStep >= 1 && animationStep < 3) ? 'yellow' : ''}}
+            >Padding</motion.li>
+            <motion.li
+              className="h-6 border border-black"
+              animate={{backgroundColor: (animationStep >= 3 && animationStep < 5) ? 'yellow' : ''}}
+            >Initial Permutation</motion.li>
+            <motion.li className="h-6 border border-black">Split Block</motion.li>
+            <motion.li className="h-6 border border-black">Expansion</motion.li>
+            <motion.li className="h-6 border border-black">Key Mixing</motion.li>
+            <motion.li className="h-6 border border-black">Substitution</motion.li>
+            <motion.li className="h-6 border border-black">Permutation</motion.li>
+
+            <li className="h-6 border border-black">Final Permutation</li>
+          </ol>
         </div>
 
-        <FlowControl
-          animationStep={animationStep}
-          stepForward={stepForward}
-          stepBackward={stepBackward}
-        />
+        <div className="flex justify-center">
+          <FlowControl
+            animationStep={animationStep}
+            stepForward={stepForward}
+            stepBackward={stepBackward}
+          />
+        </div>
       </section>
     </>
   )
