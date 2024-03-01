0384ea2ad29be26eb153bfe2f59e13fb5628fa2d
jmxor <jamesmrippeth@gmail.com>
2024-03-01 11:11:56 +0000

    Add basic ECB mode section animation

--- src/app/symmetric-cryptography/block-ciphers/modes/ecb/page.tsx
+++ src/app/symmetric-cryptography/block-ciphers/modes/ecb/page.tsx
@@ -1,9 +1,11 @@
 "use client"
 
 import {useState} from "react";
+import {motion} from "framer-motion";
 
 export default function ECBModePage() {
   const [input, setInput] = useState('');
+  const [animationStep, setAnimationStep] = useState(0);
 
   return (
     <section className="w-full flex flex-col items-center">
@@ -16,23 +18,53 @@ export default function ECBModePage() {
 
       <br/>
 
-      <div className="flex">
+      <div className="w-72 h-72 overflow-clip border border-transparent">
+        <motion.div
+          className="flex"
+          animate={{x: animationStep * -288}}
+          transition={{ease: "easeInOut"}}
+        >
+          {
+            Array(Math.ceil(input.length / 8)).fill(null).map(
+              (_, k) => <ECBSection key={k} content={input.slice(k * 8, k * 8 + 8)} />
+            )
+          }
+        </motion.div>
+      </div>
+
+      <button onClick={() => setAnimationStep(animationStep - 1)}>-</button>
+      <button onClick={() => setAnimationStep(animationStep + 1)}>+</button>
+
+    </section>
+  )
+}
+
+interface ECBSectionProps {
+  content: string
+}
+
+function ECBSection(props: ECBSectionProps) {
+  const {content} = props;
+  return (
+    <div className="flex w-72 h-72 shrink-0">
         <div className="flex flex-col justify-center">
           <div className="flex">
             <div className="w-12 h-6 border-2 border-black text-center">Key</div>
             →
           </div>
         </div>
-        <div className="flex flex-col items-center">
-          <div className="flex border border-black">
-            <div className="w-6 h-6 border border-black"></div>
-            <div className="w-6 h-6 border border-black"></div>
-            <div className="w-6 h-6 border border-black"></div>
-            <div className="w-6 h-6 border border-black"></div>
-            <div className="w-6 h-6 border border-black"></div>
-            <div className="w-6 h-6 border border-black"></div>
-            <div className="w-6 h-6 border border-black"></div>
-            <div className="w-6 h-6 border border-black"></div>
+        <div className="flex flex-col items-center justify-center">
+          <div className="relative">
+            <div className="flex border border-black">
+              {Array(8).fill(null).map(
+                (c, k) => <div key={k} className="w-6 h-6 border border-black"/>
+              )}
+            </div>
+            <div className="absolute top-0 left-0 flex border border-transparent">
+              {content.split('').map(
+                (c, k) => <div key={k} className="w-6 h-6 text-center font-mono">{c}</div>
+              )}
+            </div>
           </div>
 
           <div>↓</div>
@@ -43,19 +75,19 @@ export default function ECBModePage() {
 
           <div>↓</div>
 
-          <div className="flex border border-black">
-            <div className="w-6 h-6 border border-black"></div>
-            <div className="w-6 h-6 border border-black"></div>
-            <div className="w-6 h-6 border border-black"></div>
-            <div className="w-6 h-6 border border-black"></div>
-            <div className="w-6 h-6 border border-black"></div>
-            <div className="w-6 h-6 border border-black"></div>
-            <div className="w-6 h-6 border border-black"></div>
-            <div className="w-6 h-6 border border-black"></div>
+          <div className="relative">
+            <div className="flex border border-black">
+              {Array(8).fill(null).map(
+                (c, k) => <div key={k} className="w-6 h-6 border border-black"/>
+              )}
+            </div>
+            <div className="absolute top-0 left-0 flex border border-transparent">
+              {content.split('').map(
+                (c, k) => <div key={k} className="w-6 h-6 text-center font-mono">{c}</div>
+              )}
+            </div>
           </div>
         </div>
       </div>
-
-    </section>
   )
 }
