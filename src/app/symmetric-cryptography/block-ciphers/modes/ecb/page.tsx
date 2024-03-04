e9374dd8251baafd9fd556228cdb2a0d570cafed
jmxor <jamesmrippeth@gmail.com>
2024-03-04 09:45:45 +0000

    Add opacity animation of ecb section bit-blocks

--- src/app/symmetric-cryptography/block-ciphers/modes/ecb/page.tsx
+++ src/app/symmetric-cryptography/block-ciphers/modes/ecb/page.tsx
@@ -1,50 +1,65 @@
 "use client"
 
+import AnimationInput from "@/Components/AnimationInput";
 import {useState} from "react";
 import {motion} from "framer-motion";
 
 export default function ECBModePage() {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState('00110011');
   const [animationStep, setAnimationStep] = useState(0);
+  const stepsPerBlock = 4;
 
   return (
     <section className="w-full flex flex-col items-center">
-      <textarea
-        className="w-full border border-black resize-none"
-        value={input}
-        onChange={e => setInput(e.target.value)}
-        maxLength={64}
-      />
+     <AnimationInput
+       value={input}
+       onChange={e => setInput(e.target.value)}
+       highlightStart={0}
+       highlightEnd={8}
+     />
 
       <br/>
 
       <div className="w-72 h-72 overflow-clip border border-transparent">
         <motion.div
           className="flex"
-          animate={{x: animationStep * -288}}
+          animate={{x: Math.floor(animationStep / stepsPerBlock) * -288}}
           transition={{ease: "easeInOut"}}
         >
           {
-            Array(Math.ceil(input.length / 8)).fill(null).map(
-              (_, k) => <ECBSection key={k} content={input.slice(k * 8, k * 8 + 8)} />
+            Array(Math.ceil(input.length / 8) || 1).fill(null).map((_, k) =>
+              <ECBSection
+                key={k}
+                index={k}
+                content={input.slice(k * 8, k * 8 + 8)}
+                animationStep={animationStep}
+                stepsPerBlock={stepsPerBlock}
+              />
             )
           }
         </motion.div>
       </div>
 
-      <button onClick={() => setAnimationStep(animationStep - 1)}>-</button>
-      <button onClick={() => setAnimationStep(animationStep + 1)}>+</button>
+      <div className="flex">
+        <button onClick={() => setAnimationStep(animationStep - 1)}>-</button>
+        {animationStep}
+        <button onClick={() => setAnimationStep(animationStep + 1)}>+</button>
+      </div>
 
     </section>
   )
 }
 
 interface ECBSectionProps {
+  index: number
   content: string
+  animationStep: number
+  stepsPerBlock: number
 }
 
 function ECBSection(props: ECBSectionProps) {
-  const {content} = props;
+  const {content, animationStep, stepsPerBlock, index} = props;
+  const currentBlock = Math.floor(animationStep / stepsPerBlock) == index;
   return (
     <div className="flex w-72 h-72 shrink-0">
         <div className="flex flex-col justify-center">
@@ -60,11 +75,14 @@ function ECBSection(props: ECBSectionProps) {
                 (c, k) => <div key={k} className="w-6 h-6 border border-black"/>
               )}
             </div>
-            <div className="absolute top-0 left-0 flex border border-transparent">
+            <motion.div
+              className="absolute top-0 left-0 flex border border-transparent"
+              animate={{opacity: (currentBlock && animationStep % stepsPerBlock == 1) ? 1 : 0}}
+            >
               {content.split('').map(
                 (c, k) => <div key={k} className="w-6 h-6 text-center font-mono">{c}</div>
               )}
-            </div>
+            </motion.div>
           </div>
 
           <div>↓</div>
@@ -81,11 +99,14 @@ function ECBSection(props: ECBSectionProps) {
                 (c, k) => <div key={k} className="w-6 h-6 border border-black"/>
               )}
             </div>
-            <div className="absolute top-0 left-0 flex border border-transparent">
+            <motion.div
+              className="absolute top-0 left-0 flex border border-transparent"
+              animate={{opacity: (currentBlock && animationStep % stepsPerBlock == 2) ? 1 : 0}}
+            >
               {content.split('').map(
                 (c, k) => <div key={k} className="w-6 h-6 text-center font-mono">{c}</div>
               )}
-            </div>
+            </motion.div>
           </div>
         </div>
       </div>
