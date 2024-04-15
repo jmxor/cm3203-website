0ec7568ea58855af94dfe2a256f881857e819f85
jmxor <jamesmrippeth@gmail.com>
2024-04-15 14:47:26 +0100

    Fix error where window was undefined when getting screen dimensions

--- src/hooks/useWindowDimensions.tsx
+++ src/hooks/useWindowDimensions.tsx
@@ -1,22 +1,32 @@
 import {useEffect, useState} from "react";
 
-function getWindowDimensions() {
-  const {innerWidth: width, innerHeight: height} = window;
-  return {
-    width, height
-  }
-}
-
+/* Returns the dimensions of the current window
+ * This code was adapted from Stack Overflow post answer by giovannipds 4-12-2019
+ * accessed 15-05-2024
+ * https://stackoverflow.com/questions/36862334/get-viewport-window-height-in-reactjs
+ * Modified the handleResize function declaration to be an arrow function to conform to typescript strict mode
+ */
 export default function useWindowDimensions() {
-  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+  const hasWindow = typeof window !== 'undefined';
+
+  function getWindowDimensions() {
+    const {innerWidth: width, innerHeight: height} = window;
+    return {
+      width, height
+    }
+  }
+
+  const [windowDimensions, setWindowDimensions] = useState({width:0, height:0});
 
   useEffect(() => {
-    function handleResize() {
-      setWindowDimensions(getWindowDimensions());
-    }
+    if (hasWindow) {
+      const handleResize = () => {
+        setWindowDimensions(getWindowDimensions())
+      }
 
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize)
+      window.addEventListener('resize', handleResize);
+      return () => window.removeEventListener('resize', handleResize)
+    }
   }, []);
 
   return windowDimensions
