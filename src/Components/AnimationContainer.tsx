39f480231fe82eab224aa702f974a0e393b6895c
jmxor <jamesmrippeth@gmail.com>
2024-05-09 20:24:27 +0100

    Fix incorrect container spacing

--- src/Components/AnimationContainer.tsx
+++ src/Components/AnimationContainer.tsx
@@ -1,6 +1,6 @@
 export default function AnimationContainer({children}: Readonly<{children: React.ReactNode; }>) {
   return (
-    <figure className="w-full mx-auto max-w-96 flex flex-col gap-2 sm:mx-0 sm:float-left">
+    <figure className="w-full mx-auto max-w-96 flex flex-col gap-2 sm:ml-0 sm:mr-2 sm:float-left">
       {children}
     </figure>
   )
